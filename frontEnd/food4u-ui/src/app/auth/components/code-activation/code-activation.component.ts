c4047b40eb4dab7720cbdf6ae58a2fbab2c0d0c0
NinoosMoshi <ninos1357@yahoo.com>
2024-02-16 18:17:59 -0600

    auth done

--- frontEnd/food4u-ui/src/app/auth/components/code-activation/code-activation.component.ts
+++ frontEnd/food4u-ui/src/app/auth/components/code-activation/code-activation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-code-activation',
@@ -7,4 +10,44 @@ import { Component } from '@angular/core';
 })
 export class CodeActivationComponent {
 
+  codeForm!: FormGroup;
+
+  email:string = '';
+
+
+  constructor(private fb: FormBuilder,
+    private authService:AuthService,
+    private router: Router){}
+
+
+  ngOnInit(): void{
+    this.codeForm = this.fb.group({
+        code:[null, [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+        })
+
+    this.email = sessionStorage.getItem("emailAtive")
+  }
+
+
+  onSubmit(){
+      const code = this.codeForm.get('code')?.value;
+
+      this.authService.activeAccount(this.email,code).subscribe({
+        next:res =>{
+          if(res.result == 1){
+            this.router.navigateByUrl("/login");
+
+          }else{
+
+          }
+        },
+        error:err =>{
+
+        }
+      })
+
+  }
+
+
 }
+
